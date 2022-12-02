7fa8c2a1f8dfea630cec834290150a06bbc28938
David-Santana98 <dvs.kali.dev@gmail.com>
2022-12-02 10:15:00 -0300

    Alterando var para let

---
+++ Cap2/ex2_2.js
@@ -0,0 +1,28 @@
+// Programa JavaScript que converte a duração de um filme
+
+function converterDuracao() {
+  // cria refência aos elementos da página
+  let inTitulo = document.getElementById("inTitulo");
+  let inDuracao = document.getElementById("inDuracao");
+  let outTitulo = document.getElementById("outTitulo");
+  let outResposta = document.getElementById("outResposta");
+
+  // obtém conteúdos dos campos de entrada
+  let titulo = inTitulo.value;
+  let duracao = Number(inDuracao.value);
+
+  // arredonda para baixo o resultado da divisão
+  let horas = Math.floor(duracao / 60);
+
+  //obtém o resto da divisão entre os números
+  let minutos = duracao % 60;
+
+  // altera o conteúdo dos parágrafos de resposta
+  outTitulo.textContent = titulo;
+  outResposta.textContent = horas + " hora(s) e " + minutos + " minuto(s)";
+}
+// cria uma referência ao elemento btconverter (botão)
+let btconverter = document.getElementById("btConverter");
+
+// registra um evento associado ao botão, para carregar uma função
+btconverter.addEventListener("click", converterDuracao);
--- Cap2/js/ex2_1.js
+++ Cap2/js/ex2_1.js
@@ -1,12 +1,12 @@
 // declara a função mostraOla
 function mostrarOla() {
   // obtém o conteúdo do campo (com id=) nome
-  var nome = document.getElementById("nome").value;
+  let nome = document.getElementById("nome").value;
   // exibe no parágrafo (resposta): "Olá " e o nome informado
   document.getElementById("resposta").textContent = "Olá " + nome;
 }
 //cria uma referência ao botão (com id=) mostrar
-var mostrar = document.getElementById("mostrar");
+let mostrar = document.getElementById("mostrar");
 /*registra para o botão "mostrar" um ouvinte para o evento click,
 que ao ser clicado irá chamar a função mostrarOla
 */
