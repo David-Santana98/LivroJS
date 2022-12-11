c508224c4c441d1e1d5c7c48dee399b5940cbc57
David-Santana98 <dvs.kali.dev@gmail.com>
2022-12-11 17:46:00 -0300

    calculo_taxa_entrega

--- Cap3/js/ex3_2.js
+++ Cap3/js/ex3_2.js
@@ -40,13 +40,20 @@ function calcularPeso(){
 var btResultado = document.getElementById("btCalcular");
 btCalcular.addEventListener("click", calcularPeso);
 
-function limparCampos(){
-    document.getElementById("inNome").value = "";
-    document.getElementById("rbMasculino").checked = false;
-    document.getElementById("rbFeminino").checked = false;
-    document.getElementById("inAltura").value = "";
-    document.getElementById("outResposta").textContent = "";
+// function limparCampos(){
+//     document.getElementById("inNome").value = "";
+//     document.getElementById("rbMasculino").checked = false;
+//     document.getElementById("rbFeminino").checked = false;
+//     document.getElementById("inAltura").value = "";
+//     document.getElementById("outResposta").textContent = "";
 
+//     document.getElementById("inNome").focus();
+// }
+
+function limparCampos(){
+    // recarrega a página 
+    location.reload();
+    // posiciona( joga o foco) no elemento inNome
     document.getElementById("inNome").focus();
 }
 var btLimpar = document.getElementById("btLimpar");
