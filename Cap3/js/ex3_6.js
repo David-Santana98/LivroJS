b89ea1bcfde278a4e0a0557499b689a2c3334b44
David-Santana98 <dvs.kali.dev@gmail.com>
2022-12-12 15:02:50 -0300

    Programa Caixa eletrônico

---
+++ Cap3/js/ex3_6.js
@@ -0,0 +1,50 @@
+// Programa leia o valor de um saque e verifique se pode ser pago com as notas disponíveis) e informe o número mínimo de notas de 100,50 e 10 neceessárias para pagar esse saque
+function calcularNotas(){
+    var inSaque = document.getElementById("inSaque");
+    var outNotasCem = document.getElementById("outNotasCem");
+    var outNotasCinquenta = document.getElementById("outNotasCinquenta");
+    var outNotasDez = document.getElementById("outNotasDez");
+
+    // Limpa mensagens (caso, segunda execução)
+    outNotasCem.textContent = "";
+    outNotasCinquenta.textContent = "";
+    outNotasDez.textContent = "";
+
+    var saque = Number(inSaque.value); // converte conteúdo de campo inSaque
+
+    if (saque == 0 || isNaN(saque)){
+        alert("Informe o valor do saque corretamente"); // exibe alerta
+        inSaque.focus();
+        return;
+    }
+
+    //verifica se saque não é multiplo de 10
+    if (saque % 10 != 0) {
+        alert("Valor inválido para notas disponíveis (R$ 10, 50, 100)");
+        inSaque.focus();
+        return;
+    }
+    //Calcula notas de 100, 50, 10
+    var notasCem = Math.floor(saque / 100);
+    var resto = saque % 100;
+    var notasCinquenta = Math.floor(resto / 50);
+    resto = resto % 50;
+    var notasDez = Math.floor(resto / 10);
+
+    // exibe notas apenas se houver 
+    if(notasCem > 0) {
+        outNotasCem.textContent = "Notas de R$ 100: " + notasCem; 
+
+    }
+
+    if(notasCinquenta > 0) {
+        outNotasCinquenta.textContent = "Notas de R$ 50: " + notasCinquenta;
+    }
+
+    if (notasDez > 0) {
+        outNotasDez.textContent = "Notas de R$ 10: " + notasDez;
+    }
+
+}
+var btExibir = document.getElementById("btExibir");
+btExibir.addEventListener("click", calcularNotas);
